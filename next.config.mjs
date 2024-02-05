661e27180a334aba7e839f1175de1f241a628268
Dilan <dilan@dilanxd.com>
2024-02-05 02:19:55 -0600

    setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+
+  compiler: {
+    styledComponents: true,
+  },
+};
 
 export default nextConfig;
