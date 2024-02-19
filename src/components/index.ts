2e22eedbfa13f6e8c4031dccaf062429408d7338
Dilan <dilan@dilanxd.com>
2024-02-18 19:27:18 -0600

    add generic components

---
+++ src/components/index.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Section = styled.section`
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 16px;
+`;
+
+export const mobile = '@media (max-width: 768px)';
