cfd11315ed97c98c111e6732cb2ce39c00273c9d
stevenewald <stevenewald6@gmail.com>
2024-02-25 22:04:57 -0800

    Added TAC

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/hero";
 import Footer from "@/components/footer";
+import TAC from "@/components/tac";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <TAC />
       <Footer />
     </>
   );
