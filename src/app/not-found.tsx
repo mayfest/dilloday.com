761b8ad77969c98ca5731e5158ccef35ddf9e903
Dilan <dilan@dilanxd.com>
2024-05-29 19:48:42 -0500

    update not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,8 +5,11 @@ import { useEffect } from 'react';
 export default function NotFound() {
   useEffect(() => {
     const url = new URL(window.location.href);
+    url.hash = `#${url.pathname.slice(1)}`;
+    if (url.hash.endsWith('/')) {
+      url.hash = url.hash.slice(0, -1);
+    }
     url.pathname = '/';
-    url.hash = `#${url.pathname}`;
     window.location.replace(url.toString());
   }, []);
 
