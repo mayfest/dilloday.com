e0826b102f33eb614515820fda436a1063aafea6
Ethan Pineda <joshuapineda66@gmail.com>
2025-02-09 13:13:51 -0600

    adjusting timings on headliner spotlight

--- src/sections/lineup.tsx
+++ src/sections/lineup.tsx
@@ -287,12 +287,12 @@ const artistRowVariants = {
 };
 
 const headlinerVariants = {
-  initial: { opacity: 0, scale: 0.95 },
+  initial: { opacity: 0, scale: 0.85 },
   animate: {
     opacity: 1,
     scale: 1,
     transition: {
-      duration: 0.6,
+      duration: 0.75,
       ease: 'easeOut',
     },
   },
